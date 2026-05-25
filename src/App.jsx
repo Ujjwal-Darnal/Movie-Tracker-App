7aa953ac928e323088572d0addc61aea54a72b78
Ujjwal Darnal <59062@edu.nielsbrock.dk>
2026-05-26 01:28:18 +0200

    Set up basic Movie Tracker app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./styles/App.css";
 function App(){
   return(
-    <>
-    </>
+   <div className="app">
+    <h1>Movie Tracker App</h1>
+    <p>Your personal movie collection manager</p>
+   </div>
   )
 }
 export default App;
