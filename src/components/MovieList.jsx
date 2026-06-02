e73c0d057f3325ca4562336cc7dd93a6594d2993
Ujjwal Darnal <59062@edu.nielsbrock.dk>
2026-06-03 01:31:19 +0200

    Add empty state for movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,18 @@ import MovieCard  from "./MovieCard";
 
 
 function MovieList({movies}){
-    console.log(movies);
+   if(movies.length === 0){
+    return(
+        <div className="empty-state">
+            <h3>No Movies added yet</h3>
+            <p>Start by adding your first movie to the tracker.</p>
+        </div>
+    );
+   }
+
+
     return (
-        <div>
+        <div className="movie-list">
             {movies.map((movie)=>(
                <MovieCard key = {movie.id} movie = {movie}/>
             ))}
